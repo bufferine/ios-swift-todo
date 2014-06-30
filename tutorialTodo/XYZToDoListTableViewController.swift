7cc65619e3a68e56ce77e0cf50c0ed17d88b289d
Catherine <github@elendel.co.uk>
2014-06-30 14:17:29 +0100

    Removed unsafe option usage

--- tutorialTodo/XYZToDoListTableViewController.swift
+++ tutorialTodo/XYZToDoListTableViewController.swift
@@ -8,29 +8,27 @@ import UIKit
     // Init
     init(coder aDecoder: NSCoder!) {
         super.init(coder: aDecoder)
-        println("XYZToDoListTableViewController.init()")
     }
  
     // Called when a view under this controller's management is loaded.
     // This is the appropriate time to load in data and draw stuff.
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("viewDidLoad")
         loadInitialData()
     }
     
     // This lets another view (viewController?) revert back to this viewController, for like, going backwards or something. TODO: understand better.
     @IBAction func unwindToList(segue:UIStoryboardSegue) {
-        println("XYZToDoListTableViewController.unwindToList()")
-        var addView:XYZAddToDoItemViewController = segue.sourceViewController as XYZAddToDoItemViewController // Is this ok?
-        var item:XYZToDoItem = addView.toDoItem
-        if item.name.utf16count > 0 {
-            toDoItems.append(item)
-            tableView.reloadData()
-        } else {
-            println("No Item Name")
+        let s:AnyObject = segue.sourceViewController
+        switch s {
+            case let c as XYZAddToDoItemViewController where c.toDoItem.name.utf16count > 0 :  toDoItems.append(c.toDoItem)
+                tableView.reloadData()
+            default: println("Nothing To Add")// do nothing
         }
+
     }
+
+
 
     // Load up the array with some fake data
     func loadInitialData() {
@@ -47,11 +45,6 @@ import UIKit
     }
     
     // How many items are in each section?
-    // TODO: I do not understand this syntax: `numberOfRowsInSection section: Int`
-    //          is this a delegate? A named delegate?
-    // Read: override this function tableView
-    //       The function takes a tableview, which is an optional wrapped UITableView
-    //       It also takes a numberOfRowsInSection section, which is an integer. ???
     override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
         return toDoItems.count
     }
@@ -59,26 +52,32 @@ import UIKit
     // Get the cell data at a row index
     override func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath: NSIndexPath?) -> UITableViewCell? {
         
-//        let cell = tableView.dequeueReusableCellWithIdentifier("ListPrototypeCell", forIndexPath: indexPath) // boilerplate fails
-        
-        // http://stackoverflow.com/questions/24022763/uitableview-in-swift
-        let cell = tableView!.dequeueReusableCellWithIdentifier("ListPrototypeCell") as? UITableViewCell
-        let item = toDoItems[indexPath!.row]
-        cell!.textLabel.text = item.name
-        if item.completed {
-            cell!.accessoryType = UITableViewCellAccessoryType.Checkmark
-        } else {
-            cell!.accessoryType = UITableViewCellAccessoryType.None
+        let cell:AnyObject? = tableView?.dequeueReusableCellWithIdentifier("ListPrototypeCell")
+        if (indexPath?.row){
+            let item = toDoItems[indexPath!.row]
+            if (cell){
+                let c = cell as UITableViewCell
+                c.accessoryType = item.completed ? UITableViewCellAccessoryType.Checkmark : UITableViewCellAccessoryType.None
+                    c.textLabel.text = item.name
+                    return c
+            }else{
+                return nil
+            }
+        }else{
+            return nil
         }
-        return cell
     }
     
     // when a table view item is selected
     override func tableView(tableView: UITableView?, didSelectRowAtIndexPath indexPath: NSIndexPath?) {
-        tableView!.deselectRowAtIndexPath(indexPath, animated: false) // Deselect this cell, because it was just selected and we don't want to keep that state. :)
-        let tappedItem = toDoItems[indexPath!.row]                    // Which item was tapped?
-        tappedItem.completed = !tappedItem.completed                  // Toggle the completion state
-        tableView!.reloadRowsAtIndexPaths([ indexPath! ], withRowAnimation: UITableViewRowAnimation.None) // Tell the table view to render the changed row
+        if let tv = tableView {
+            tv.deselectRowAtIndexPath(indexPath, animated: false) 
+            if let i = indexPath{
+                let tappedItem = toDoItems[i.row]                    // Which item was tapped?
+                tappedItem.completed = !tappedItem.completed                  // Toggle the completion state
+                tv.reloadRowsAtIndexPaths([i], withRowAnimation: UITableViewRowAnimation.None) // Tell the table view to render the changed row
+            }
+        }
     }
 }
 
